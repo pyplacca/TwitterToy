ec0600e48b5aa4951955251597010728ce5319de
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-11 05:57:56 +0000

    refactor file (see commit description)
    1. Rename apitokens.py to auth.py
    2. create api handler class to root manage api calls

---
+++ auth.py
@@ -0,0 +1,30 @@
+from os import environ as env
+import tweepy
+
+
+API_KEY = env.get('TWITTER_API_KEY')
+API_SECRET_KEY = env.get('TWITTER_API_SECRET_KEY')
+ACCESS_TOKEN = env.get('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = env.get('TWITTER_ACCESS_TOKEN_SECRET')
+BEARER_TOKEN = env.get('TWITTER_BEARER_TOKEN')
+
+
+class TweetAPIHandler:
+    # authenticate user
+    def __init__(self):
+        self.auth = tweepy.OAuthHandler(
+            API_KEY,
+            API_SECRET_KEY
+        )
+        self.auth.set_access_token(
+            ACCESS_TOKEN,
+            ACCESS_TOKEN_SECRET
+        )
+
+    @property
+    def api(self):
+        # configure api
+        return tweepy.API(self.auth, retry_count=5, retry_delay=10)
+
+
+API = TweetAPIHandler().api
