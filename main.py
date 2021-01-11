7bcf86b8927f5bb292b7b7d1f816f3919087a8a2
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-11 06:19:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import tweepy
-from database import DatabaseManager, default_database
-# from decouple import config
-from os import environ as env
-import time
+from logging import INFO, ERROR
+from utils import DB_PATH, DatabaseManager, TweetLogger
+from auth import API
+import asyncio
 
-print(env.get('TWITTER_API_KEY'))
-# tweet_db.use_table('tweets', time='text', url='text', id='int')
-# tweet_db = DatabaseManager(default_database)
-# tweet_db.use_table('tweets')
-# print(tweet_db.get(id='13bIigY3N'))
-# tweet_db.insert(time=time.asctime(), url='https://twitter.com/0jfn208jns12noadoa90e', id='13bIigY3N')
 
+api = API
+# user = api.me()
+
+logger = TweetLogger()
+# logger.log_message(msg='Tweeting - Test log 2')
+
+tweet_db = DatabaseManager(DB_PATH)
+# tweet_db.use_table('Tweets', timestamp='text', id='int', text='text', quote='int')
+
+# print(api.get_settings())
