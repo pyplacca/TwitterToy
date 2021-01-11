a1a07fc065de338a689757c2452fc1f901a0cd78
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-11 06:18:56 +0000

    Create __version__.py

---
+++ __version__.py
@@ -0,0 +1,11 @@
+
+
+__title__ = 'ddoi'
+__description__ = 'Twitter bot for tweeting inspirational quotes'
+# __url__ = ''
+__version__ = '1.0.0'
+__author__ = 'David Placca'
+__author_email__ = 'dhegreatprosperity10@gmail.com'
+__author_github__ = ''
+# __license__ = ''
+__copyright__ = 'Copyright 2021 David Placca'
