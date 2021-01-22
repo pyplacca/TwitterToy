a323997b067bdb31b276855ed98794199ed60bb9
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-22 12:33:53 +0000

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,4 @@
-from .db import DatabaseManager, DB_PATH
+from .db import DatabaseManager
 from .logger import TweetLogger
 from .fetch import get_quote
-
-
-class QueryRow(dict):
-    def __init__(self, keys, values):
-        arg_zip = zip(keys, values)
-        super().__init__(dict(arg_zip))
-        for key, value in arg_zip:
-            try:
-                getattr(self, key)
-                key = f'{key}_'
-            except AttributeError:
-                pass
-            setattr(self, key, value)
+from .misc import *
