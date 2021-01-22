a58ea8c5843b531143291df80e5718e54873e219
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-22 12:35:03 +0000

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,22 +1,11 @@
 from __version__ import __title__
+from __config__ import ROOT_DIR
 import logging
 import time
 import os
 
 
-LOG_FILE = os.path.join(
-    os.environ.get('USERPROFILE'),
-    f'.{__title__}',
-    'tweets.log'
-)
-
-try:
-    open(LOG_FILE)
-except FileNotFoundError:
-    try:
-        os.mkdir(os.path.dirname(LOG_FILE))
-    except FileExistsError:
-        pass
+LOG_FILE = os.path.join(ROOT_DIR, 'tweets.log')
 
 LOG_HANDLER = logging.FileHandler(LOG_FILE, encoding='utf-8')
 
@@ -28,7 +17,7 @@ class TweetLogger(logging.Logger):
         super().__init__(f'{__title__}.TweetLogger', DEFAULT_LEVEL)
         self.addHandler(LOG_HANDLER)
 
-    def log_message(self, level: int = DEFAULT_LEVEL, msg: str = '') -> None:
+    async def log_message(self, level: int = DEFAULT_LEVEL, msg: str = '') -> None:
         log_level = level
         message = logging.BASIC_FORMAT % ({
             'levelname': logging.getLevelName(log_level),
