0b4d34cbacfb36088d9052264d54ef77c7ba2908
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-11 18:54:17 +0000

    Update __version__.py

--- __version__.py
+++ __version__.py
@@ -1,11 +1,9 @@
 
 
 __title__ = 'ddoi'
-__description__ = 'Twitter bot for tweeting inspirational quotes'
-# __url__ = ''
+__description__ = 'A friendly bot that tweets inspirational quotes'
+__url__ = 'https://twitter.com/dailydoseofvim'
 __version__ = '1.0.0'
 __author__ = 'David Placca'
 __author_email__ = 'dhegreatprosperity10@gmail.com'
-__author_github__ = ''
-# __license__ = ''
 __copyright__ = 'Copyright 2021 David Placca'
