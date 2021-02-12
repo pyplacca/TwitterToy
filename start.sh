f6d32905510b1330ae3445588da5b2ec4bc677d1
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-12 12:13:32 +0000

    update shell script to use Python 3 on glitch servers

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
-# tell glitch to start the app from the main.py file.
-python3 main.py
+# shell command to enable bot to run on glitch servers
+
+MD5="$(md5sum requirements.txt | cut -f1 -d' ')-site-packages"
+
+if ! [ -d ".data/$MD5" ]; then
+    rm -rf .data/*-site-packages
+    pip3 install -U -r requirements.txt -t ".data/$MD5"
+fi
+
+exec env PYTHONPATH="$PWD/.data/$MD5" python3 main.py
